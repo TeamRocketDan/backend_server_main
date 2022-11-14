48bb02e4c0be4a314eb186e4ce944ef900b7909b
DESKTOP-BNVQCL4\82109 <amyyzzin@naver.com>
2022-11-14 22:47:41 +0900

    feed_rcate1/feed_rcate2 값이 null인 경우 전체 조회로 수정

--- src/main/java/com/rocket/user/userfeed/repository/query/FeedQueryRepository.java
+++ src/main/java/com/rocket/user/userfeed/repository/query/FeedQueryRepository.java
@@ -1,23 +1,5 @@
 package com.rocket.user.userfeed.repository.query;
 
-import com.querydsl.core.types.Expression;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.rocket.user.user.entity.User;
-import com.rocket.user.userfeed.dto.*;
-import com.rocket.user.userfeed.entity.FeedImage;
-import com.rocket.user.userfeed.entity.QFeedCommentLike;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.ObjectUtils;
-
-import java.util.List;
-
 import static com.querydsl.jpa.JPAExpressions.select;
 import static com.querydsl.jpa.JPAExpressions.selectOne;
 import static com.rocket.user.user.entity.QUser.user;
@@ -27,6 +9,26 @@ import static com.rocket.user.userfeed.entity.QFeedCommentLike.feedCommentLike;
 import static com.rocket.user.userfeed.entity.QFeedImage.feedImage;
 import static com.rocket.user.userfeed.entity.QFeedLike.feedLike;
 
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.rocket.user.user.entity.User;
+import com.rocket.user.userfeed.dto.FeedCommentQDto;
+import com.rocket.user.userfeed.dto.FeedListDto;
+import com.rocket.user.userfeed.dto.FeedSearchCondition;
+import com.rocket.user.userfeed.dto.QFeedCommentQDto;
+import com.rocket.user.userfeed.dto.QFeedListDto;
+import com.rocket.user.userfeed.entity.FeedImage;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ObjectUtils;
+
 @Repository
 @RequiredArgsConstructor
 public class FeedQueryRepository {
@@ -34,126 +36,126 @@ public class FeedQueryRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     public Page<FeedListDto> findByRcate1EqualsAndRcate2EqualsOrderByCreatedAtDesc
-            (FeedSearchCondition searchCondition,
-             User userEntity,
-             Pageable pageable,
-             boolean isMain) {
+        (FeedSearchCondition searchCondition,
+            User userEntity,
+            Pageable pageable,
+            boolean isMain) {
 
         List<FeedListDto> content = jpaQueryFactory
-                .select(
-                    new QFeedListDto(
-                            user.id,
-                            feed.id,
-                            user.profileImage,
-                            user.username,
-                            user.nickname.coalesce(""),
-                            user.email,
-                            feed.title,
-                            feed.content,
-                            feed.rcate1,
-                            feed.rcate2,
-                            feed.longitude,
-                            feed.latitude,
-                            isLike(userEntity),
-                            select(feedLike.count())
-                                    .from(feedLike)
-                                    .where(feedLike.feed.eq(feed)),
-                            select(feedComment.count())
-                                    .from(feedComment)
-                                    .where(feedComment.feed.eq(feed))
-                    )
+            .select(
+                new QFeedListDto(
+                    user.id,
+                    feed.id,
+                    user.profileImage,
+                    user.username,
+                    user.nickname.coalesce(""),
+                    user.email,
+                    feed.title,
+                    feed.content,
+                    feed.rcate1,
+                    feed.rcate2,
+                    feed.longitude,
+                    feed.latitude,
+                    isLike(userEntity),
+                    select(feedLike.count())
+                        .from(feedLike)
+                        .where(feedLike.feed.eq(feed)),
+                    select(feedComment.count())
+                        .from(feedComment)
+                        .where(feedComment.feed.eq(feed))
                 )
-                .from(feed)
-                .innerJoin(feed.user, user)
-                .where(
-                        eqRcate(searchCondition),
-                        isMain(isMain, userEntity),
-                        feed.deletedAt.isNull()
-                )
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .orderBy(feed.id.desc())
-                .fetch();
+            )
+            .from(feed)
+            .innerJoin(feed.user, user)
+            .where(
+                eqRcate(searchCondition),
+                isMain(isMain, userEntity),
+                feed.deletedAt.isNull()
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(feed.id.desc())
+            .fetch();
 
         JPAQuery<Long> countQuery = jpaQueryFactory
-                .select(feed.count())
-                .from(feed)
-                .where(
-                        eqRcate(searchCondition),
-                        isMain(isMain, userEntity),
-                        feed.deletedAt.isNull()
-                );
+            .select(feed.count())
+            .from(feed)
+            .where(
+                eqRcate(searchCondition),
+                isMain(isMain, userEntity),
+                feed.deletedAt.isNull()
+            );
 
         return PageableExecutionUtils.getPage(
-                content,
-                pageable,
-                countQuery::fetchOne
+            content,
+            pageable,
+            countQuery::fetchOne
         );
     }
 
     public List<FeedImage> findByAllFeedIn(List<Long> feedsId) {
         return jpaQueryFactory
-                .select(feedImage)
-                .from(feedImage)
-                .innerJoin(feedImage.feed, feed)
-                .where(feedImage.feed.id.in(feedsId))
-                .orderBy(feedImage.feed.id.desc())
-                .fetch();
+            .select(feedImage)
+            .from(feedImage)
+            .innerJoin(feedImage.feed, feed)
+            .where(feedImage.feed.id.in(feedsId))
+            .orderBy(feedImage.feed.id.desc())
+            .fetch();
     }
 
     public Page<FeedCommentQDto> feedCommentFindByFeedId(
-            Long feedId,
-            User userEntity,
-            Pageable pageable) {
+        Long feedId,
+        User userEntity,
+        Pageable pageable) {
 
         List<FeedCommentQDto> content = jpaQueryFactory
-                .select(
-                    new QFeedCommentQDto(
-                            user.id,
-                            user.profileImage,
-                            user.nickname,
-                            user.username,
-                            user.email,
-                            feedComment.id,
-                            feedComment.feed.id,
-                            feedComment.comment,
-                            feedComment.createdAt,
-                            feedComment.updatedAt,
-                            feedCommentLike.count(),
-                            selectOne()
-                                    .from(feedCommentLike)
-                                    .where(
-                                            feedCommentLike.feedComment.eq(feedComment),
-                                            feedCommentLike.user.eq(userEntity)
-                                    ).exists()
+            .select(
+                new QFeedCommentQDto(
+                    user.id,
+                    user.profileImage,
+                    user.nickname,
+                    user.username,
+                    user.email,
+                    feedComment.id,
+                    feedComment.feed.id,
+                    feedComment.comment,
+                    feedComment.createdAt,
+                    feedComment.updatedAt,
+                    feedCommentLike.count(),
+                    selectOne()
+                        .from(feedCommentLike)
+                        .where(
+                            feedCommentLike.feedComment.eq(feedComment),
+                            feedCommentLike.user.eq(userEntity)
+                        ).exists()
 
-                    )
                 )
-                .from(feedComment)
-                .innerJoin(feedComment.user, user)
-                .leftJoin(feedCommentLike).on(feedComment.id.eq(feedCommentLike.feedComment.id))
-                .where(
-                        feedComment.feed.id.eq(feedId),
-                        feedComment.deletedAt.isNull()
-                )
-                .groupBy(feedComment)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .orderBy(feedComment.createdAt.desc())
-                .fetch();
+            )
+            .from(feedComment)
+            .innerJoin(feedComment.user, user)
+            .leftJoin(feedCommentLike).on(feedComment.id.eq(feedCommentLike.feedComment.id))
+            .where(
+                feedComment.feed.id.eq(feedId),
+                feedComment.deletedAt.isNull()
+            )
+            .groupBy(feedComment)
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(feedComment.createdAt.desc())
+            .fetch();
 
         JPAQuery<Long> countQuery = jpaQueryFactory
-                .select(feedComment.count())
-                .from(feedComment)
-                .where(
-                        feedComment.feed.id.eq(feedId),
-                        feedComment.deletedAt.isNull()
-                );
+            .select(feedComment.count())
+            .from(feedComment)
+            .where(
+                feedComment.feed.id.eq(feedId),
+                feedComment.deletedAt.isNull()
+            );
 
         return PageableExecutionUtils.getPage(
-                content,
-                pageable,
-                countQuery::fetchOne
+            content,
+            pageable,
+            countQuery::fetchOne
         );
     }
 
@@ -164,11 +166,11 @@ public class FeedQueryRepository {
         }
 
         return selectOne()
-                .from(feedLike)
-                .where(
-                        feedLike.feed.eq(feed),
-                        feedLikeUserEqUser(user)
-                ).exists();
+            .from(feedLike)
+            .where(
+                feedLike.feed.eq(feed),
+                feedLikeUserEqUser(user)
+            ).exists();
     }
 
     private BooleanExpression feedUserEqUser(User user) {
@@ -184,7 +186,15 @@ public class FeedQueryRepository {
     }
 
     private BooleanExpression eqRcate(FeedSearchCondition feedSearchCondition) {
+
+        if (feedSearchCondition.getRcate1() == null) {
+            return null;
+        } else if (feedSearchCondition.getRcate1() != null
+            && feedSearchCondition.getRcate2() == null) {
+            return feed.rcate1.eq(feedSearchCondition.getRcate1());
+        }
+
         return feed.rcate1.eq(feedSearchCondition.getRcate1())
-                .and(feed.rcate2.eq(feedSearchCondition.getRcate2()));
+            .and(feed.rcate2.eq(feedSearchCondition.getRcate2()));
     }
 }
